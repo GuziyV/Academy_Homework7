8cbd1a784d65d177edf2d2972e01239d6ba3c4c3
GuziyV <volodyaguziy3@gmail.com>
2018-07-18 07:51:17 +0300

    third task

---
+++ Shared/JsonObjects/PilotJson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shared.JsonObjects
+{
+    public class PilotJson
+    {
+        public int Id { get; set; }
+
+        public int CrewId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Exp { get; set; }
+
+        public DateTime birthDate { get; set; }
+    }
+}
